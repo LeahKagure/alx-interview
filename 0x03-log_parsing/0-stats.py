b6da5c48705b3717117737426af4a49c6564f2d0
root <leah.kagure98@gmail.com>
2023-01-17 15:46:57 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,50 @@
-#!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+# Initialize variables to store metrics
 total_size = 0
-counter = 0
+status_codes = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+# Read input line by line
+line_count = 0
+for line in sys.stdin:
+    # Check if line matches input format
+    parts = line.split()
+    if len(parts) != 9:
+        continue
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    # Extract file size and status code
+    try:
+        file_size = int(parts[8])
+        status_code = int(parts[7])
+    except ValueError:
+        continue
 
-except Exception as err:
-    pass
+    # Update metrics
+    total_size += file_size
+    if status_code in status_codes:
+        status_codes[status_code] += 1
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    line_count += 1
+    # Print statistics every 10 lines
+    if line_count % 10 == 0:
+        print("Total file size: ", total_size)
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] > 0:
+                print(f"{code}: {status_codes[code]}")
+
+# Print final statistics on keyboard interruption
+except KeyboardInterrupt:
+    print("Total file size: ", total_size)
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
